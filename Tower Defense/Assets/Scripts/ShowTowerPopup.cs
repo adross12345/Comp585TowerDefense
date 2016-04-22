46362b7f80dffb34139211b40a947c5bf691e616
Stephen Yan <sjyan@cs.unc.edu>
2016-04-21 22:51:40 -0400

    Deleted toggle groups ally units can be trained too as well. Also cleaned up variables

--- Tower Defense/Assets/Scripts/ShowTowerPopup.cs
+++ Tower Defense/Assets/Scripts/ShowTowerPopup.cs
@@ -13,24 +13,24 @@ public class ShowTowerPopup : EditorWindow {
 	string priceWarning = "You do not have enough money!";
 	int price = 0;
 	int nodeCost = 0;
-	bool isLearned;
+	bool isTarget;
 	private static CastleHealth castle;
-	List<bool> learnedUnits = new List<bool>();
+	List<bool> leaisTargetUnits = new List<bool>();
 	float noiseScale = 1.0f;
 	public string[] options = new string[] {"Grayscale", "Full Color", "Color Histogram"};
 	public int nodeType = 0;
 	NeuralNode.NodeType neuralNodeType;
 
-	bool isLearned1;
-	bool isLearned2;
-	bool isLearned3;
-	bool isLearned4;
-	bool isLearned5;
-	bool isLearned6;
-	bool isLearned7;
-	bool isLearned8;
-	bool isLearned9;
-	bool isLearned10;
+	bool isTarget1;
+	bool isTarget2;
+	bool isTarget3;
+	bool isTarget4;
+	bool isTarget5;
+	bool isTarget6;
+	bool isTarget7;
+	bool isTarget8;
+	bool isTarget9;
+	bool isTarget10;
 
 	int quantity1;
 	int quantity2;
@@ -43,11 +43,6 @@ public class ShowTowerPopup : EditorWindow {
 	int quantity9;
 	int quantity10;
 
-	bool isTrainingEmpty = true;
-
-	public static string towerName = "";
-	public Transform trainable;
-	public Texture2D towerLabel;
 	public static GameObject tower = null;
 	private static CannonFire cFire;
 
@@ -94,94 +89,76 @@ public class ShowTowerPopup : EditorWindow {
 
 
 	void OnGUI() {
-		
-		// Set up interface
+
 		// Still need to add pictures
+		// Noise slider
 		noiseScale = EditorGUILayout.Slider ("Noise", noiseScale, 1, 0);
+
+		// Node options
 		nodeType = EditorGUILayout.Popup(nodeType, options);
 		InstantiatePrimitive ();
 
+		// Unit target and quantity fields
 		EditorGUILayout.BeginHorizontal();
-		isLearned1 = EditorGUILayout.Toggle ("Unit 1 (Ally1)", isLearned1);
-		EditorGUI.BeginDisabledGroup (!isLearned1);
+		isTarget1 = EditorGUILayout.Toggle ("Unit 1 (Ally1)", isTarget1);
 		quantity1 = EditorGUILayout.IntField (quantity1);
-		EditorGUI.EndDisabledGroup();
 		EditorGUILayout.EndHorizontal();
 
 		EditorGUILayout.BeginHorizontal();
-		isLearned2 = EditorGUILayout.Toggle ("Unit 2 (Ally2)", isLearned2);
-		EditorGUI.BeginDisabledGroup (!isLearned2);
+		isTarget2 = EditorGUILayout.Toggle ("Unit 2 (Ally2)", isTarget2);
 		quantity2 = EditorGUILayout.IntField (quantity2);
-		EditorGUI.EndDisabledGroup();
 		EditorGUILayout.EndHorizontal ();
 
 		EditorGUILayout.BeginHorizontal();
-		isLearned3 = EditorGUILayout.Toggle ("Unit 3 (Enemy1)", isLearned3);
-		EditorGUI.BeginDisabledGroup (!isLearned3);
+		isTarget3 = EditorGUILayout.Toggle ("Unit 3 (Enemy1)", isTarget3);
 		quantity3 = EditorGUILayout.IntField (quantity3);
-		EditorGUI.EndDisabledGroup();
 		EditorGUILayout.EndHorizontal ();
 
 		EditorGUILayout.BeginHorizontal();
-		isLearned4 = EditorGUILayout.Toggle ("Unit 4 (Enemy2)", isLearned4);
-		EditorGUI.BeginDisabledGroup (!isLearned4);
+		isTarget4 = EditorGUILayout.Toggle ("Unit 4 (Enemy2)", isTarget4);
 		quantity4 = EditorGUILayout.IntField (quantity4);
-		EditorGUI.EndDisabledGroup();
 		EditorGUILayout.EndHorizontal ();
 
 		EditorGUILayout.BeginHorizontal();
-		isLearned5 = EditorGUILayout.Toggle ("Unit 5 (EnemyCamo)", isLearned5);
-		EditorGUI.BeginDisabledGroup (!isLearned5);
+		isTarget5 = EditorGUILayout.Toggle ("Unit 5 (EnemyCamo)", isTarget5);
 		quantity5 = EditorGUILayout.IntField (quantity5);
-		EditorGUI.EndDisabledGroup();
 		EditorGUILayout.EndHorizontal ();
 
 		EditorGUILayout.BeginHorizontal();
-		isLearned6 = EditorGUILayout.Toggle ("Unit 6 (EnemyHydra)", isLearned6);
-		EditorGUI.BeginDisabledGroup (!isLearned6);
+		isTarget6 = EditorGUILayout.Toggle ("Unit 6 (EnemyHydra)", isTarget6);
 		quantity6 = EditorGUILayout.IntField (quantity6);
-		EditorGUI.EndDisabledGroup();
 		EditorGUILayout.EndHorizontal ();
 
 		EditorGUILayout.BeginHorizontal();
-		isLearned7 = EditorGUILayout.Toggle ("Unit 7 (EnemySnake)", isLearned7);
-		EditorGUI.BeginDisabledGroup (!isLearned7);
+		isTarget7 = EditorGUILayout.Toggle ("Unit 7 (EnemySnake)", isTarget7);
 		quantity7 = EditorGUILayout.IntField (quantity7);
-		EditorGUI.EndDisabledGroup();
 		EditorGUILayout.EndHorizontal ();
 
 		EditorGUILayout.BeginHorizontal();
-		isLearned8 = EditorGUILayout.Toggle ("Unit 8 (EnemyAnimated)", isLearned8);
-		EditorGUI.BeginDisabledGroup (!isLearned8);
+		isTarget8 = EditorGUILayout.Toggle ("Unit 8 (EnemyAnimated)", isTarget8);
 		quantity8 = EditorGUILayout.IntField (quantity8);
-		EditorGUI.EndDisabledGroup();
 		EditorGUILayout.EndHorizontal ();
 
 		EditorGUILayout.BeginHorizontal();
-		isLearned9 = EditorGUILayout.Toggle ("Unit 9 (EnemyAISwapper)", isLearned9);
-		EditorGUI.BeginDisabledGroup (!isLearned9);
+		isTarget9 = EditorGUILayout.Toggle ("Unit 9 (EnemyAISwapper)", isTarget9);
 		quantity9 = EditorGUILayout.IntField (quantity9);
-		EditorGUI.EndDisabledGroup();
 		EditorGUILayout.EndHorizontal ();
 
 		EditorGUILayout.BeginHorizontal();
-		isLearned10 = EditorGUILayout.Toggle ("Unit 10 (EnemyAIZeroer)", isLearned10);
-		EditorGUI.BeginDisabledGroup (!isLearned10);
+		isTarget10 = EditorGUILayout.Toggle ("Unit 10 (EnemyAIZeroer)", isTarget10);
 		quantity10 = EditorGUILayout.IntField (quantity10);
-		EditorGUI.EndDisabledGroup();
 		EditorGUILayout.EndHorizontal ();
 
+		// Training cost field and control
 		GUIStyle red = new GUIStyle(EditorStyles.label);
 		GUIStyle black = new GUIStyle(EditorStyles.label);
 		red.normal.textColor = Color.red;
 		black.normal.textColor = Color.black;
 		EditorGUILayout.LabelField ("Cost of Training:", "$" + price.ToString(), 
 			castle.canPurchase(price) ? black : red);
-		int sumQuant = (isLearned1 ? quantity1 : 0) + (isLearned2 ? quantity2 : 0)
-		               + (isLearned3 ? quantity3 : 0) + (isLearned4 ? quantity4 : 0) +
-		               (isLearned5 ? quantity5 : 0) + (isLearned6 ? quantity6 : 0) +
-		               (isLearned7 ? quantity7 : 0) + (isLearned8 ? quantity8 : 0) +
-		               (isLearned9 ? quantity9 : 0) + (isLearned10 ? quantity10 : 0);
+		int sumQuant = quantity1 + quantity2 + quantity3 + quantity4 + quantity5
+		               + quantity6 + quantity7 + quantity8 + quantity9 + quantity10;
+
 		if (sumQuant <= 10) {
 			price = (int)(10 * (1 - noiseScale) + nodeCost);
 		} else {
@@ -195,6 +172,7 @@ public class ShowTowerPopup : EditorWindow {
 		Repaint ();
 		*/
 
+		// Train button
 		GUILayout.Space(30);
 		if (GUILayout.Button ("Train!")) {
 
@@ -206,72 +184,62 @@ public class ShowTowerPopup : EditorWindow {
 				
 				for (int i = 0; i < quantity1; i++) {
 					Unit unit = uGen.MakeUnit (false, 0, farOff, noiseScale, true);
-					node.AddToTrainingSet (unit, isLearned1);
+					node.AddToTrainingSet (unit, isTarget1);
 					unit.DestroyMe ();
-					isTrainingEmpty = false;
 				}
 
 				for (int i = 0; i < quantity2; i++) {
 					Unit unit = uGen.MakeUnit (false, 1, farOff, noiseScale, true);
-					node.AddToTrainingSet (unit, isLearned2);
+					node.AddToTrainingSet (unit, isTarget2);
 					unit.DestroyMe ();
-					isTrainingEmpty = false;
 				}
 
 				for (int i = 0; i < quantity3; i++) {
 					Unit unit = uGen.MakeUnit (true, 0, farOff, noiseScale, true);
-					node.AddToTrainingSet (unit, isLearned3);
+					node.AddToTrainingSet (unit, isTarget3);
 					unit.DestroyMe ();
-					isTrainingEmpty = false;
 				}
 
 				for (int i = 0; i < quantity4; i++) {
 					Unit unit = uGen.MakeUnit (true, 1, farOff, noiseScale, true);
-					node.AddToTrainingSet (unit, isLearned4);
+					node.AddToTrainingSet (unit, isTarget4);
 					unit.DestroyMe ();
-					isTrainingEmpty = false;
 				}
 
 				for (int i = 0; i < quantity5; i++) {
 					Unit unit = uGen.MakeUnit (true, 2, farOff, noiseScale, true);
-					node.AddToTrainingSet (unit, isLearned5);
+					node.AddToTrainingSet (unit, isTarget5);
 					unit.DestroyMe ();
-					isTrainingEmpty = false;
 				}
 
 				for (int i = 0; i < quantity6; i++) {
 					Unit unit = uGen.MakeUnit (true, 4, farOff, noiseScale, true);
-					node.AddToTrainingSet (unit, isLearned6);
+					node.AddToTrainingSet (unit, isTarget6);
 					unit.DestroyMe ();
-					isTrainingEmpty = false;
 				}
 
 				for (int i = 0; i < quantity7; i++) {
 					Unit unit = uGen.MakeUnit (true, 6, farOff, noiseScale, true);
-					node.AddToTrainingSet (unit, isLearned6);
+					node.AddToTrainingSet (unit, isTarget6);
 					unit.DestroyMe ();
-					isTrainingEmpty = false;
 				}
 
 				for (int i = 0; i < quantity8; i++) {
 					Unit unit = uGen.MakeUnit (true, 5, farOff, noiseScale, true);
-					node.AddToTrainingSet (unit, isLearned8);
+					node.AddToTrainingSet (unit, isTarget8);
 					unit.DestroyMe ();
-					isTrainingEmpty = false;
 				}
 
 				for (int i = 0; i < quantity9; i++) {
 					Unit unit = uGen.MakeUnit (true, 9, farOff, noiseScale, true);
-					node.AddToTrainingSet (unit, isLearned9);
+					node.AddToTrainingSet (unit, isTarget9);
 					unit.DestroyMe ();
-					isTrainingEmpty = false;
 				}
 
 				for (int i = 0; i < quantity10; i++) {
 					Unit unit = uGen.MakeUnit (true, 3, farOff, noiseScale, true);
-					node.AddToTrainingSet (unit, isLearned10);
+					node.AddToTrainingSet (unit, isTarget10);
 					unit.DestroyMe ();
-					isTrainingEmpty = false;
 				}
 
 				node.LearnUnits ();
@@ -284,7 +252,6 @@ public class ShowTowerPopup : EditorWindow {
 				ShowNotification(new GUIContent(priceWarning));
 			} else if (sumQuant == 0) {
 				// alert that no training is going to be done
-				// default
 				Debug.Log("Training set is empty!");
 				noDataWarning = EditorGUILayout.TextField (noDataWarning);
 				ShowNotification(new GUIContent(noDataWarning));
