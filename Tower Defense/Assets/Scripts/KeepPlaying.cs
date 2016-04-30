ab1b727028dee28c318306a67b4d9708e2ebb327
Stephen Yan <sjyan@cs.unc.edu>
2016-04-29 20:41:52 -0400

    Added background music and script to keep music playing

---
+++ Tower Defense/Assets/Scripts/KeepPlaying.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeepPlaying : MonoBehaviour {
+
+	private static KeepPlaying instance = null;
+
+	public static KeepPlaying Instance {
+		get { return instance; }
+	}
+
+	void Awake() {
+		if (instance != null && instance != this) {
+			Destroy(this.gameObject);
+			return;
+		} else {
+			instance = this;
+		}
+
+		DontDestroyOnLoad(this.gameObject);
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
